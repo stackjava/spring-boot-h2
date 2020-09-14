6e993fde5e43e6dbcc8bbce89f079a515027afc3
cuongth <cuong.9312@gmail.com>
2020-09-14 17:30:21 +0700

    init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO 
+	tbl_user (username, password)
+VALUES
+  	('stackjava', 'stackjava'),
+  	('admin', 'admin1234'),
+  	('root', 'root');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS tbl_user;
+ 
+CREATE TABLE tbl_user (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  username VARCHAR(250) NOT NULL,
+  password VARCHAR(250) NOT NULL
+);
